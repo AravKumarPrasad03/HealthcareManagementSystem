36b0a4fdd4eaf02a5fbbb93c79a9db0dc71eb8da
AravKumarPrasad <arav3737@gmail.com>
2024-09-22 16:13:52 +0530

    Change the name in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ export default withSentryConfig(
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
 
-    org: "arav-au",
+    org: "Arav",
     project: "care-pulse",
 
     // Only print logs for uploading source maps in CI
@@ -48,7 +48,7 @@ export default withSentryConfig(
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
 
-    org: "arav-au",
+    org: "Arav",
     project: "javascript-nextjs",
     sentryUrl: "https://sentry.io/",
 
